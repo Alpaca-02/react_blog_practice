1194bf72a8470c1760db7ed311dfeb6e42c2bd78
Alpaca-02 <babb8960@naver.com>
2023-06-15 09:41:15 +0900

    블로그 기초작업

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <div className="black-nav"> 
+          <h4>블로그</h4>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
